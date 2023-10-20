a6408af13334774ce12a2f349e704ca0af7cf7ae
thurstonzd <thurstonzd@g.cofc.edu>
2023-10-19 20:19:42 -0400

    switch __init__.py back to penguins.py

--- data_raw/penguins.py
+++ data_raw/penguins.py
@@ -1,4 +1,7 @@
+import re
+
 import pandas as pd
+from skimpy import clean_columns
 
 uri_adelie = "https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-pal.219.3&entityid=002f3893385f710df69eeebe893144ff"
 uri_gentoo = "https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-pal.220.3&entityid=e03b43c924f226486f2f0ab6709d2381"
@@ -8,7 +11,13 @@ uris = [uri_chinstrap, uri_adelie, uri_gentoo]
 data_frames = []
 
 for uri in uris:
-    df = pd.read_csv(uri)
+    df = pd.read_csv(uri, keep_default_na=False, na_values=["", "NA", "."])
     data_frames.append(df)
 
-penguins = pd.concat(data_frames)
+def species_short(obs):
+    return re.sub(r'\s.*', '', obs.species)
+
+df = clean_columns(pd.concat(data_frames))
+penguins = (df
+    .assign(species=species_short)
+)
